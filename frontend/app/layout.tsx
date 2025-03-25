8410ef51803f8c86f27f04d028cb1737aeed5c3a
Joshua Zobule <joshwhizkid@gmail.com>
2025-03-26 10:06:44 +1100

    testing out primary buttono

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -5,11 +5,13 @@ import "./globals.css";
 const inter = Inter({
   variable: '--font-inter',
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 })
 
 const rubik = Rubik({
   variable: '--font-rubik',
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
 })
 
 
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,11 @@
+import PrimaryButton from "@/app/ui/PrimaryButton";
+
 export default function Home() {
     return (
-      <h1>this is the Home page</h1>
+      <>
+        <h1>this is the Home page</h1>
+        <PrimaryButton text="testing"/>
+      </>
+      
   );
 }
