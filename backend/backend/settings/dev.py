24ecb4747d9c77858dc49f2b7e3dde6ff22b75d0
Joshua Zobule <joshwhizkid@gmail.com>
2025-05-29 13:56:50 +1100

    testing out building of docker image

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -4,7 +4,9 @@ from .base import *
 DEBUG = os.environ.get("DEBUG") == "True"
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-a5_^a2v*j8_&3kqyw9!beypyr85z9*e0$@bn5h3p308mamf95"
+)
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
