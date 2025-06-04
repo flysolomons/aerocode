4c1929360a9d643a83467d6ae0e7559015fcd06d
Joshua Zobule <joshwhizkid@gmail.com>
2025-06-04 20:37:04 +1100

    added sample form inputs to booking widget

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
 export const stripHtmlTags = (html: string | undefined) => {
   if (!html) return "";
   return html.replace(/<[^>]*>/g, "");
