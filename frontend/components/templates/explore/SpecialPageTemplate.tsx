3b19c0b3cfb45ea9b04c4578bd5dea5cf746cec4
Joshua Zobule <joshwhizkid@gmail.com>
2025-06-30 10:08:12 +1100

    specials index page and backend endpoint change

--- frontend/components/templates/explore/SpecialPageTemplate.tsx
+++ frontend/components/templates/explore/SpecialPageTemplate.tsx
@@ -107,6 +107,15 @@ export default function SpecialPageTemplate({
             )} */}
 
             {/* Special Info Section */}
+
+            {/* THINGS TO INCLUDE: 
+              - Sales period
+              - Travel period
+              - Percentage discount
+              - Trip Type (e.g., one-way, round-trip)
+          
+          
+          */}
             <div className="bg-white border border-gray-200 rounded-xl shadow-sm p-6 mb-4">
               <div className="flex justify-between items-center mb-4">
                 <h2 className="text-2xl font-bold text-blue-500">
