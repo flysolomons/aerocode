6b4c1ba0ab8bcf26f24efaf9c97bcaf24aa56964
Joshua Zobule <joshwhizkid@gmail.com>
2025-07-03 10:02:08 +1100

    updated route card

--- frontend/components/ui/cards/RouteCard.tsx
+++ frontend/components/ui/cards/RouteCard.tsx
@@ -16,14 +16,14 @@ function RouteCard({ origin, destination, url }: RouteCardProps) {
           height="24"
           viewBox="0 0 24 24"
           fill="none"
-          stroke="currentColor"
-          strokeWidth="1"
+          stroke="#212061"
+          strokeWidth="1.5"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="lucide lucide-map-pin-icon lucide-map-pin text-blue-500 flex-shrink-0"
+          className="lucide lucide-map-pin-icon lucide-map-pin flex-shrink-0"
         >
-          <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
-          <circle cx="12" cy="10" r="3" />
+          <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/>
+          <circle cx="12" cy="10" r="3"/>
         </svg>
         <span className="font-medium text-sm sm:text-base lg:text-base pl-2 ">
           {origin} to {destination}
