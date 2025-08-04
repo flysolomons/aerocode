5a348e9d336a9e9c12d281dc25e74283d85e1d58
Geolynx Saemane <gsaemane@genisys.com.sb>
2025-08-04 15:15:50 +1100

    Update ReadyToFLy Book now section
    Create a component for ReadToFLy Book now button

---
+++ frontend/components/layout/sections/ReadyToFly.tsx
@@ -0,0 +1,42 @@
+
+import PrimaryButton
+ from "@/components/ui/buttons/PrimaryButton";
+interface ReadyToFlyProps {
+  buttonText: string;
+  description?: string;
+}
+export default function ReadyToFly({
+    buttonText,
+    description
+}: ReadyToFlyProps) {
+  return (
+    <>
+      {/* Ready to fly section */}
+      <div className="text-center space-y-6 py-14 rounded-2xl border-4 border-white bg-[url('https://images.pexels.com/photos/1058959/pexels-photo-1058959.jpeg')] bg-cover  bg-center ">
+            <h2 className="text-4xl sm:text-xl lg:text-5xl font-semibold text-white">
+              Ready to Fly?
+            </h2>
+            {description && (<p className="text-white">{description}</p>)}
+            <div className="flex justify-center">
+            
+              <PrimaryButton
+                text={buttonText}
+                onClick={() => {
+                  const widget = document.querySelector(
+                    ".stripped-booking-widget"
+                  );
+                  if (widget) {
+                    widget.scrollIntoView({ behavior: "smooth" });
+                  } else {
+                    // Fallback to scroll to top where the hero with booking widget is
+                    window.scrollTo({ top: 0, behavior: "smooth" });
+                  }
+                }}
+              />
+
+              
+            </div>
+        </div>
+    </>
+  );
+}
