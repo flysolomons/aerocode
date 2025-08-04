4384ee626948715c819ee96e850151f3320dd5ee
Geolynx Saemane <gsaemane@genisys.com.sb>
2025-08-04 15:16:27 +1100

    Update RouteSpecialSection

--- frontend/components/layout/sections/RouteSpecialSection.tsx
+++ frontend/components/layout/sections/RouteSpecialSection.tsx
@@ -24,7 +24,7 @@ export default function RouteSpecialSection({
               <h2 className="text-2xl md:text-3xl lg:text-3xl text-center font-bold text-blue-500">
                 {heading}
               </h2>
-              <p className="block text-center text-sm md:text-base lg:text-base text-gray-700 leading-relaxed max-w-2xl md:max-w-4xl mx-auto">
+              <p className="block text-center text-sm md:text-base lg:text-base text-gray-600 leading-relaxed max-w-2xl md:max-w-4xl mx-auto">
                 {description}
               </p>
             </>
