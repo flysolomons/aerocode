aa1310a46ec865bd292356dc30b08188e0702ae1
Geolynx Saemane <gsaemane@genisys.com.sb>
2025-08-04 15:17:06 +1100

    Update GenericCardVariant

--- frontend/components/ui/cards/GenericCardVariants/GenericCardFull.tsx
+++ frontend/components/ui/cards/GenericCardVariants/GenericCardFull.tsx
@@ -5,27 +5,29 @@ interface GenericCardFullProps {
   title: string;
   image: string;
   url: string; // Changed from optional to required
-  subTitle?: string;//option from backend
+  subTitle?: any;//option from backend
 }
 
 function GenericCardFull({ title,subTitle, image, url }: GenericCardFullProps) {
   return (
     <Link href={url} className="block hover:opacity-90 transition-opacity">
-      <div className="relative rounded-2xl sm:rounded-3xl overflow-hidden w-full h-48 sm:h-56 md:h-64 lg:h-72 xl:h-80 shadow-lg sm:shadow-xl bg-white">
-        <div className="absolute inset-0  "></div>
+      <div className="relative rounded-2xl lg:rounded-3xl overflow-hidden w-full h-48  md:h-64 lg:h-72 xl:h-80 shadow-lg lg:shadow-xl bg-white border-4 border-white">
+        <div className="absolute inset-0 "></div>
+       
         <Image
           src={image}
           alt={title}
           width={500}
           height={500}
-          className="w-full h-full object-cover p-2 rounded-3xl"
+          className="w-full h-full object-cover rounded-3xl "
         />
-        <div className="absolute  text-white z-20  bottom-1 left-1 w-full ">
-            <div className="flex flex-col p-4 rounded-3xl">
-                <span className="text-lg sm:text-xl md:text-2xl lg:text-2xl font-semibold  ">
+       
+        <div className="absolute  text-white z-20 bottom-0 w-full  rounded-b-2xl p-4 bg-gradient-to-t from-black to-transparent">
+            <div className="flex flex-col rounded-3xl">
+                <span className="text-xl md:text-2xl lg:text-2xl font-semibold  ">
                     {title}
                 </span>
-                <span>
+                <span className="text-blue-50">
                   {subTitle || ''}
                 </span>
 
