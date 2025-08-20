3a0dbb3a0e4d6493b89d3a638d4a9509fb09f3eb
Joshua Zobule <joshwhizkid@gmail.com>
2025-08-20 11:43:17 +1100

    updated layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -34,7 +34,12 @@ const rubik = Rubik({
 
 // Local fonts with Next.js optimization
 const veneer = localFont({
-  src: "./fonts/VENEER.OTF",
+  src: [
+    {
+      path: "../public/fonts/VENEER.OTF",
+      style: "normal",
+    },
+  ],
   variable: "--font-veneer",
   display: "swap",
   preload: true, // Preload this font since it's used in hero sections
@@ -43,7 +48,7 @@ const veneer = localFont({
 const edmondsans = localFont({
   src: [
     {
-      path: "./fonts/Edmondsans-Medium.otf",
+      path: "../public/fonts/Edmondsans-Medium.otf",
       weight: "500",
       style: "normal",
     },
